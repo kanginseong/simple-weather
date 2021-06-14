c06509e0d2d90a4cef792a7e55988b6a17b3ab90
insung <inh3011@gmail.com>
2021-06-14 18:38:16 +0900

    Getting the Condition Names:

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Alert} from 'react-native';
 import Loading from "./Loading";
+import Weather from "./Weather";
 import * as Location from "expo-location";
 import axios from "axios";
 import config from "./config";
@@ -8,7 +9,6 @@ import config from "./config";
 const API_KEY = config;
 
 export default class extends React.Component {
-
   state = {
     isLoading: true
   };
@@ -16,28 +16,28 @@ export default class extends React.Component {
     const {data} = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    console.log(data);
-  }
+    this.setState({isLoading: false, temp: data.main.temp});
+  };
   getLocation = async() => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {
-        coords:{latitude, longitude}
+        coords: {latitude, longitude}
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
       this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("can's find you.");
     }
-  }
+  };
 
   componentDidMount(){
     this.getLocation();
   }
 
   render(){
-    const {isLoading} = this.state;
-    return isLoading ? <Loading /> : null;
+    const {isLoading, temp} = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
   }
 }
 
